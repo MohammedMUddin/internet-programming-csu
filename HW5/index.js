1cc879fc198616ad0abb3041d9fe06358cbf4592
MohammedMUddin <misbah1033@gmail.com>
2021-04-10 15:23:03 -0400

    backend and sql setup complete

--- HW5/index.js
+++ HW5/index.js
@@ -3,11 +3,26 @@ const mysql  = require('mysql')
 
 const app = express()
 
+const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'hw5'
+  });
+   
+connection.connect();
+
+connection.query('SELECT * FROM `course`', function (error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+  });
+   
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.listen('3000', () => {
-    console.log('Server is running...')
+    console.log('Server is running on 3000...')
 })
 
