e6e4cec806bae3211013b2fd672efc70bcdd90b0
MohammedMUddin <misbah1033@gmail.com>
2021-04-10 23:05:12 -0400

    completed search query

--- HW5/index.js
+++ HW5/index.js
@@ -1,32 +1,35 @@
+// importing packages
 const express  = require('express')
 const mysql  = require('mysql')
 
+// initializing express object 
 const app = express()
-app.use(express.urlencoded()); 
 
+// decoding data received from html form
+app.use(express.urlencoded())
+
+// establishing connection
 const connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : '',
     database : 'hw5'
+  });  
+connection.connect()
+
+//grabbing form input and searching database for inputted data
+app.post('/', function(req, res) {
+  var sqlQuery =  `
+                  SELECT student_name 
+                  FROM course 
+                  WHERE student_name = '${req.body.query}'`
+  connection.query(sqlQuery, function (error, results, fields) {
+    if (error) throw error
+    res.send('You sent the name "' + results[0].student_name + '".')
   });
-   
-connection.connect();
-
-connection.query('SELECT * FROM `course`', function (error, results, fields) {
-    if (error) throw error;
-    console.log(results);
-  });
-   
-
-app.get('/hello', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.post('/myaction', function(req, res) {
-  res.send('You sent the name "' + req.body.query + '".');
 });
 
+// check to see whether server running or not
 app.listen('3000', () => {
     console.log('Server is running on 3000...')
 })
@@ -34,5 +37,6 @@ app.listen('3000', () => {
 /*
 TODO
 1. Make Search query through database
-2. Make Input XML into Database
+2. insert Input XML into Database
+3. Restructure output to html or ejs?
 */
